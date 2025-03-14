8a4d80aac7f5dd13a031661cb03afda6c45eae35
Ilyass Ezzam <ilyassezzam1337@gmail.com>
2025-03-14 03:26:13 +0000

    Done

---
+++ philo_bonus/include/philo.h
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iezzam <iezzam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 23:23:14 by iezzam            #+#    #+#             */
+/*   Updated: 2025/03/11 23:16:32 by iezzam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <semaphore.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+
+typedef struct s_philosopher
+{
+	int				id;
+	pid_t			pid;
+	int				meals_eaten;
+	long long		last_meal_time;
+	struct s_philo	*shared;
+}		t_philosopher;
+
+typedef struct s_philo
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eat_count;
+	long long		start_time;
+	int				stop_simulation;
+	sem_t			*forks_sem;
+	sem_t			*print_sem;
+	sem_t			*meal_check_sem;
+	sem_t			*done_sem;
+	sem_t			*all_ate_sem;
+	t_philosopher	*philosophers;
+	pid_t			*pids;
+}		t_philo;
+
+// Process and monitor functions
+void		*monitor_routine(void *arg);
+void		philosopher_routine(t_philosopher *philo);
+
+// Time and utility functions
+long long	get_time(void);
+void		smart_sleep(long long time);
+void		print_status(t_philosopher *philo, char *msg);
+
+// Initialization
+int			initialize_philosophers(t_philo *philo);
+int			philo_init(t_philo *philo);
+int			parse_philo(int ac, char **av, t_philo *philo);
+
+// Monitor
+int			start_simulation(t_philo *philo);
+
+// Semaphore utilities
+void		create_semaphores(t_philo *philo);
+void		close_semaphores(t_philo *philo);
+
+// Cleanup function
+void		philo_cleanup(t_philo *philo);
+// lib
+double		ft_atof(const char *str);
+void		ft_putchar(char c);
+void		ft_putstr(char *s);
+int			ft_strcmp(const char *s1, const char *s2);
+size_t		ft_strlen(const char *s);
+
+#endif
---
+++ philo_bonus/src/components/philo_routine.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_routine.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iezzam <iezzam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 13:20:58 by iezzam            #+#    #+#             */
+/*   Updated: 2025/03/14 02:05:07 by iezzam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/philo.h"
+
+void *death_monitor(void *arg)
+{
+	t_philosopher *philo;
+	long long current_time;
+
+	philo = (t_philosopher *)arg;
+	while (1)
+	{
+		sem_wait(philo->shared->meal_check_sem);
+		current_time = get_time();
+		if (current_time - philo->last_meal_time > philo->shared->time_to_die)
+		{
+			print_status(philo, "died");
+			sem_post(philo->shared->done_sem);
+			return NULL;
+		}
+		sem_post(philo->shared->meal_check_sem);
+		usleep(1000);
+	}
+	return NULL;
+}
+
+void	eat(t_philosopher *philo)
+{
+	sem_wait(philo->shared->forks_sem);
+	print_status(philo, "has taken a fork");
+	sem_wait(philo->shared->forks_sem);
+	print_status(philo, "has taken a fork");
+	sem_wait(philo->shared->meal_check_sem);
+	print_status(philo, "is eating");
+	philo->last_meal_time = get_time();
+	philo->meals_eaten++;
+	if (philo->shared->eat_count > 0 && \
+			philo->meals_eaten >= philo->shared->eat_count)
+		sem_post(philo->shared->all_ate_sem);
+	sem_post(philo->shared->meal_check_sem);
+	smart_sleep(philo->shared->time_to_eat);
+	sem_post(philo->shared->forks_sem);
+	sem_post(philo->shared->forks_sem);
+}
+
+void philosopher_routine(t_philosopher *philo)
+{
+	pthread_t death_thread;
+
+	if (pthread_create(&death_thread, NULL, death_monitor, philo) != 0)
+	{
+		sem_close(philo->shared->forks_sem);
+		sem_close(philo->shared->print_sem);
+		sem_close(philo->shared->meal_check_sem);
+		sem_close(philo->shared->done_sem);
+		sem_close(philo->shared->all_ate_sem);
+		exit(1);
+	}
+	pthread_detach(death_thread);
+	if (philo->id % 2 == 0)
+		usleep(1000 * philo->shared->time_to_eat / 2);
+	while (1)
+	{
+		eat(philo);
+		print_status(philo, "is sleeping");
+		smart_sleep(philo->shared->time_to_sleep);
+		print_status(philo, "is thinking");
+	}
+}
